f738dffa48e7be1bc55bdb42bc27213864e58768
jmartinez360 <mrjem1360@gmail.com>
2019-05-08 15:21:43 +0200

    Creación de la estructura de colores
    Creamos la estructura base para los colores de cada carta
    Tenemos cartas de color básico y cartas premium
    estos colores se ponen en un enum y este es quien decide que colores corresponden a cada carta dependiendo el tipo

--- app/src/main/java/com/dev/rickandmortydeadoralive/ui/activities/MainActivity.kt
+++ app/src/main/java/com/dev/rickandmortydeadoralive/ui/activities/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.dev.rickandmortydeadoralive.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.dev.rickandmortydeadoralive.Injector
 import com.dev.rickandmortydeadoralive.R
 import com.dev.rickandmortydeadoralive.models.Character
 import com.dev.rickandmortydeadoralive.ui.presenters.CharactersDeckPresenter
 import com.dev.rickandmortydeadoralive.ui.views.CharactersDeckView
+import com.dev.rickandmortydeadoralive.utils.cardColors.CardType
+import com.dev.rickandmortydeadoralive.utils.cardColors.GradientCardColor
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity(), CharactersDeckView {
 
@@ -22,8 +30,42 @@ class MainActivity : AppCompatActivity(), CharactersDeckView {
 
         Injector.obtain(this)!!.inject(this)
 
+        val gradientCardType = CardType.GRADIENT_CARD.cardColor as GradientCardColor
+
         presenter.view = this
         presenter.loadCharacters()
+
+        val characterId = findViewById<TextView>(R.id.character_id)
+        val infoContainer = findViewById<LinearLayout>(R.id.info_container)
+        val animationDrawable = AnimationDrawable()
+
+        for (x in 0 until gradientCardType.bodyColor.size) {
+            val gradientDrawable = GradientDrawable()
+
+            var indexFirstColor = x
+            var indexSecondColor = x + 1
+
+            if (x == gradientCardType.bodyColor.size - 1) {
+                indexFirstColor = x
+                indexSecondColor = 0
+            }
+
+            gradientDrawable.colors = intArrayOf(
+                Color.parseColor(gradientCardType.bodyColor[indexFirstColor]),
+                Color.parseColor(gradientCardType.bodyColor[indexSecondColor])
+            )
+
+            gradientDrawable.gradientType = GradientDrawable.LINEAR_GRADIENT
+            gradientDrawable.orientation = GradientDrawable.Orientation.BR_TL
+
+            animationDrawable.addFrame(gradientDrawable, 2000)
+        }
+
+        characterId.background = animationDrawable
+        infoContainer.background = animationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(2000)
+        animationDrawable.start()
     }
 
     override fun showCharacters(characterList: List<Character>) {
---
+++ app/src/main/java/com/dev/rickandmortydeadoralive/utils/cardColors/BasicCardColor.kt
@@ -0,0 +1,5 @@
+package com.dev.rickandmortydeadoralive.utils.cardColors
+
+class BasicCardColor(val bodyColor: String, val separatorColor: String,
+                     val boxColor: String, val titleColor: String,
+                     val bodyTextColor: String) : CardColor
